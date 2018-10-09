552d3b6162903c63ac57cf944c4c7a4f7eaacf37
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2018-10-09 17:50:56 -0400

    Funcionamiento minimo y pruebas

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,5 +1,5 @@
 use chrono::Duration as Delta;
-use chrono::{DateTime, Local, Utc};
+use chrono::{DateTime, Local, NaiveDateTime, Utc};
 use clap::ArgMatches;
 use humantime::parse_duration;
 use rusoto_core::Region;
@@ -8,16 +8,13 @@ use std::result::Result;
 use std::str::FromStr;
 use std::time::Duration;
 
-fn calculate_start_time(delta: Duration) -> Option<i64> {
+fn calculate_start_time(from: DateTime<Local>, delta: Duration) -> Option<i64> {
     let chrono_delta = Delta::from_std(delta).unwrap();
-    let start_time = Local::now().checked_sub_signed(chrono_delta).unwrap();
+    let start_time = from.checked_sub_signed(chrono_delta).unwrap();
     // Amazon uses time in UTC so we have to convert
     let utc_time = DateTime::<Utc>::from_utc(start_time.naive_utc(), Utc);
     return Some(utc_time.timestamp());
 }
-
-//[#test]
-
 
 fn create_filter_request(
     group: &str,
@@ -31,22 +28,51 @@ fn create_filter_request(
     return req;
 }
 
+fn print_date(time: Option<i64>) -> String {
+    match time {
+        Some(x) => NaiveDateTime::from_timestamp(x / 1000, 0)
+            .format("%Y-%m-%d %H:%M:%S")
+            .to_string()
+            .to_owned(),
+        None => "".to_owned(),
+    }
+}
+
 fn fetch_logs(region: Region, group: &str, since: Duration) -> Result<(), String> {
     let client = CloudWatchLogsClient::new(region);
-    let delta = calculate_start_time(since);
+    let delta = calculate_start_time(Local::now(), since);
     let req = create_filter_request(group, delta, None);
     // right now
     let logs = client.filter_log_events(req).sync().unwrap();
     let events = logs.events.unwrap();
     for event in events.into_iter() {
+        println!("-----------------------------------------------");
         println!(
             "{} {} {}",
-            event.event_id.unwrap(),
-            event.timestamp.unwrap(),
-            event.message.unwrap()
+            print_date(event.timestamp),
+            event.message.unwrap(),
+            Local::now().to_rfc3339()
         );
+        println!("-----------------------------------------------");
     }
     return Ok(());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::{Duration, Local};
+    use humantime::parse_duration;
+
+    #[test]
+    fn test_calculate_start_time() {
+        let local = Local::now();
+        let duration = parse_duration("1d").unwrap();
+        assert_eq!(
+            calculate_start_time(local, duration).unwrap(),
+            (local - Duration::days(1)).timestamp()
+        )
+    }
 }
 
 pub fn run(matches: ArgMatches) -> Result<(), String> {
