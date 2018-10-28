c765e31bd0c39e58ee20da882374ec1628b0b29f
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2018-10-28 19:51:26 -0400

    Adds a timeout parameter

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,18 @@ use clap::{Arg, App};
 use cmd::run;
 
 mod cmd;
-// fn run(matches: ArgMatches)
-
-// }
 
 fn main() {
     let matches = App::new("awstail")
         .version("0.1.0")
         .author("Yoandy Rodriguez <yoandy.rmartinez@gmail.com>")
         .about("like tail -f for AWS Cloudwatch")
+        .arg(Arg::with_name("timeout")
+             .short("t")
+             .required(false)
+             .takes_value(true)
+             .value_name("TIMEOUT")
+             .help("Set connection timeout"))
         .arg(Arg::with_name("group")
              .short("g")
              .required(true)
@@ -35,7 +38,6 @@ fn main() {
              .help("Take logs since given time (defaults to 5 minutes)"))
         .get_matches();
     if let Err(_e) = run(matches) {
-        println!("Tuvimos un error");
         std::process::exit(1);
     }
 }
