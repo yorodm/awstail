a838db491cf0e134c63e6f4f83ade547283d2548
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2019-12-27 13:45:04 -0500

    feat: refine UX, add Ctrl+C support and "watch log group" feature

--- src/cmd.rs
+++ src/cmd.rs
@@ -11,12 +11,17 @@ use std::result::Result;
 use std::str::FromStr;
 use std::time::Duration;
 
+enum AWSResponse {
+    Token(String),
+    LastLog(Option<i64>),
+}
+
 fn calculate_start_time(from: DateTime<Local>, delta: Duration) -> Option<i64> {
     let chrono_delta = Delta::from_std(delta).unwrap();
     let start_time = from.checked_sub_signed(chrono_delta).unwrap();
     // Amazon uses time in UTC so we have to convert
     let utc_time = DateTime::<Utc>::from_utc(start_time.naive_utc(), Utc);
-    return Some(utc_time.timestamp() * 1000);
+    return Some(utc_time.timestamp_millis());
 }
 
 fn create_filter_request(
@@ -27,6 +32,19 @@ fn create_filter_request(
     let mut req = FilterLogEventsRequest::default();
     let delta = calculate_start_time(Local::now(), start);
     req.start_time = delta;
+    req.next_token = token;
+    req.limit = Some(100);
+    req.log_group_name = group.to_string();
+    return req;
+}
+
+fn create_filter_from_timestamp(
+    group: &str,
+    start: Option<i64>,
+    token: Option<String>,
+) -> FilterLogEventsRequest {
+    let mut req = FilterLogEventsRequest::default();
+    req.start_time = start;
     req.next_token = token;
     req.limit = Some(100);
     req.log_group_name = group.to_string();
@@ -46,22 +64,30 @@ fn fetch_logs(
     client: &CloudWatchLogsClient,
     req: FilterLogEventsRequest,
     timeout: Duration,
-) -> Option<String> {
+) -> AWSResponse {
     let response = client
-        .filter_log_events(req)
+        .filter_log_events(req.clone()) // we may need this later
         .with_timeout(timeout)
         .sync()
         .unwrap();
     let events = response.events.unwrap();
     let green = Style::new().green();
-    for event in events.into_iter() {
+    let mut last: Option<i64> = None;
+    for event in events {
         println!(
             "{} {}",
             green.apply_to(print_date(event.timestamp)),
             event.message.unwrap().trim(),
         );
+        last = event.timestamp
     }
-    return response.next_token;
+    match response.next_token {
+        Some(x) => AWSResponse::Token(x),
+        None => match last {
+            Some(t) => AWSResponse::LastLog(Some(t)),
+            None => AWSResponse::LastLog(req.start_time),
+        },
+    }
 }
 
 pub fn client_with_profile(name: &str, region: Region) -> CloudWatchLogsClient {
@@ -84,7 +110,7 @@ mod tests {
         let duration = parse_duration("1d").unwrap();
         assert_eq!(
             calculate_start_time(local, duration).unwrap(),
-            (local - Duration::days(1)).timestamp() * 1000
+            (local - Duration::days(1)).timestamp_millis()
         )
     }
 }
@@ -107,14 +133,24 @@ pub fn run(matches: ArgMatches) -> Result<(), String> {
         Some(m) => client_with_profile(m, region.unwrap()),
         None => CloudWatchLogsClient::new(region.unwrap()),
     };
+    let sleep_for = match matches.value_of("watch") {
+        Some(m) => parse_duration(m),
+        None => parse_duration("10s"),
+    };
     let mut token: Option<String> = None;
     let mut req = create_filter_request(group, mtime.unwrap(), token);
     loop {
         match fetch_logs(&client, req, timeout.unwrap()) {
-            Some(x) => token = Some(x),
-            None => break,
+            AWSResponse::Token(x) => {
+                token = Some(x);
+                req = create_filter_request(group, mtime.unwrap(), token);
+            }
+            AWSResponse::LastLog(t) => {
+                token = None;
+                req = create_filter_from_timestamp(group, t, token);
+                std::thread::sleep(sleep_for.unwrap());
+            }
         };
-        req = create_filter_request(group, mtime.unwrap(), token);
     }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 use clap::{App, Arg};
 use cmd::run;
-
+use ctrlc;
 mod cmd;
 
 fn main() {
+    ctrlc::set_handler(move || std::process::exit(0))
+        .expect("Could not set Ctrl+C handler...bailing out");
     let matches = App::new("awstail")
-        .version("0.1.0")
+        .version("0.2.0")
         .author("Yoandy Rodriguez <yoandy.rmartinez@gmail.com>")
         .about("like tail -f for AWS Cloudwatch")
         .arg(
             Arg::with_name("group")
-                .short("g")
                 .required(true)
                 .takes_value(true)
                 .value_name("LOG_GROUP")
-                .help("Log group name"),
+                .help("Log group name")
+                .index(1),
         )
         .arg(
             Arg::with_name("region")
@@ -36,6 +38,13 @@ fn main() {
                 .required(false)
                 .value_name("SINCE")
                 .help("Take logs since given time (defaults to 5 minutes)"),
+        )
+        .arg(
+            Arg::with_name("watch")
+                .short("w")
+                .required(false)
+                .value_name("WATCH")
+                .help("Keep watching for new logs every n seconds (defaults to 10)"),
         )
         .get_matches();
     if let Err(_e) = run(matches) {
