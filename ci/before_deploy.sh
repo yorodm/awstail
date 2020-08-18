090b05c070978c7e3860b16a51384ee1e32f2b37
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2020-08-18 15:50:51 -0400

    fix: having some problems with the CI

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,7 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin awstail --target $TARGET --release -- -C lto
+    cross rustc --bin awstail --target $TARGET --release
 
     # TODO Update this to package the right artifacts
 	case $TARGET in
